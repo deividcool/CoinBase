f5d4636dd0f0b4918659594a9fc7db9be4c727c8
deividcool <velozadeivid@gmail.com>
2023-06-26 05:41:38 -0500

    flatlist

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,21 +1,83 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, Image } from 'react-native';
+import Colors from '../constants/Colors';
+import CBButton from '../components/CBButton';
 
+import { WatchlistState } from '../store/reducers/watchlist';
+import { useSelector, useDispatch } from 'react-redux';
+import * as watchlistActions from '../store/actions/watchlist';
+
+
+import Whatchlist from '../components/Whatchlist';
+
+interface RootState {
+    watchlist: WatchlistState
+}
 
 const Home = () => {
+
+    const watchlistData = useSelector(
+        (state: RootState) => state.watchlist.watchlistData
+    )
+
+    const dispatch = useDispatch();
+
+    const loadData = () => {
+        try {
+            dispatch(watchlistActions.fetchCoinData());
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+
+    useEffect(() => {
+        loadData();
+    },[]);
+
+
     return (
-        <View style={styles.screen}>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView
+                contentContainerStyle={{ alignItems: 'center' }}
+
+            >
+                <Image
+                    source={{ uri: 'https://i.imgur.com/9EEaSaS.png' }}
+                    style={styles.image}
+
+                />
+                <Text style={styles.title}>Welcome to Coinbase!</Text>
+                <Text style={styles.subTitle}>Make your first investment today</Text>
+                <CBButton title='Get Start' />
+                <Whatchlist coinData={watchlistData}/>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    screen: {
+    container: {
         flex: 1,
-        justifyContent: 'center', 
-        alignItems: 'center',
-    }
+        justifyContent: 'flex-start',
+        backgroundColor: '#fff',
+    },
+    image: {
+        height: 250,
+        width: 150,
+        marginTop: 40,
+    },
+    title: {
+        fontSize: 21,
+        fontWeight: '600',
+        marginBottom: 8,
+        letterSpacing: 0.5,
+    },
+    subTitle: {
+        fontSize: 17,
+        marginBottom: 24,
+        color: Colors.subtitle,
+    },
 }
 )
 
